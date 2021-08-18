83d62cfcee800d08c2e63953fa3171381a28354b
Tarun Batra <tarun.batra00@gmail.com>
2021-08-19 00:16:49 +0200

    Add a timeout to getting providers

--- src/lib/ipfs.js
+++ src/lib/ipfs.js
@@ -26,25 +26,30 @@ export default class IpfsLib {
 		console.log(`Collecting datapoints of the content for cid: ${cid}...`)
 		const data = this.ipfsInstance.ls(input, {});
 		let dataPoints = 0;
-		const timeout = createTimeout(2 * 60) // timeout of 2 minutes
+		const dataPointsTimeout = createTimeout(2 * 60) // timeout of 2 minutes
 		for await (let d of data) {
 			dataPoints++;
 			console.log(`Got datapoint #${dataPoints} for cid: ${cid}`, d)
 			if (dataPoints >= 50) {
 				console.log(`Enough datapoints collected for cid: ${cid}`)
 				break;
-			} else if (timeout.triggered) {
+			} else if (dataPointsTimeout.triggered) {
 				console.log(`Datapoints collection timedout for cid: ${cid}`)
 				break;
 			}
 		}
-		await sleep(5);
+		await sleep(5); // sleep for 5 seconds
+		const providerTimeout = createTimeout(2 * 60) // timeout of 2 minutes
 		const res = this.ipfsInstance.dht.findProvs(cid);
 		console.log(`Getting providers for cid: ${cid}...`)
 		const peers = []
 		for await (let peer of res) {
 			peers.push(peer)
 			console.log(`Got provider #${peers.length}: ${peer.id}`)
+			if (providerTimeout.triggered) {
+				console.log(`Getting providers timedout for cid: ${cid}`)
+				break;
+			}
 		}
 		console.log(`Total providers for cid: ${cid}`, peers)
 		return peers
