29e6ce08965bf5d513c65d607f9f24cc7aaa6df0
Tarun Batra <tarun.batra00@gmail.com>
2021-08-18 22:19:24 +0200

    Update title regularly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,48 @@
 import './App.css';
 import CIDForm from '../CIDForm/CIDForm';
+import { getRandomBetween } from '../../lib/utils';
+import { useEffect, useState } from 'react'
+
+const titles = [
+  'de-centralized ?',
+  'p2p ?',
+  'web3 ?',
+  'distributed ?',
+  'censorship-resistant ?'
+]
+let timeout = null
+
+function updateTitleRegularly (setTitle) {
+  if (timeout) return
+  timeout = setInterval(() => {
+    const randomIndex = getRandomBetween(0, titles.length)
+    setTitle(titles[randomIndex] || titles[0])
+  }, 10 * 1000)
+}
 
 function App() {
+  const [title, setTitle] = useState(titles[0])
+  useEffect(() => {
+    updateTitleRegularly(setTitle)
+  }, [])
   return (
     <div className='App App-header'>
-      <h1 className='head'> de-centralized?</h1>
+      <h1 className='head'>{title}</h1>
       <h1>Check distribution rating of any IPFS content</h1>
       <p>
         <strong>What makes data on a p2p network distibuted?</strong> Is it still p2p if
         there's only one <em>peer</em> server acting as the source for the data?
-        We consider number of servers providing the data (and
+        This portal checks number of servers providing the data (and
         <abbr title='JS-IPFS currently does not provide IP addresses of providers'> in future </abbr>
-        other things like countries, pinning, etc.) to rate the content.
+        other things like countries, pinning, etc.) to rate the content right from your browser tab.
       </p>
       <CIDForm />
 
       <footer>
-        <sub>© Tarun Batra</sub>
+        <sub>
+          { /* eslint-disable-next-line react/jsx-no-target-blank */ }
+          © {new Date().getFullYear()} <a href='https://tarunbatra.com' target='_blank' rel='author'>Tarun Batra</a>
+        </sub>
       </footer>
     </div>
   );
--- src/components/CIDForm/CIDForm.js
+++ src/components/CIDForm/CIDForm.js
@@ -1,5 +1,5 @@
 import './CIDForm.css'
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import IpfsLib from '../../lib/ipfs';
 import Rating from '../Rating/Rating';
 
